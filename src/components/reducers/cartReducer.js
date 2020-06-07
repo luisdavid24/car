709673dcd8ae0c26b7cd4d6ff6934bca84b88060
Luis david <david12.29@hotmail.com>
2020-06-07 18:01:24 -0500

    Estoy trabajando en cartReducer

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -1,3 +1,8 @@
+import Item1 from '../../images/bananas.jpg'
+import Item2 from '../../images/fresas.jpg'
+import Item3 from '../../images/magos.jpg'
+import Item4 from '../../images/peras.jpg'
+import Item5 from '../../images/sandia.jpg'
 const cartReducer=(state=initState,action)=>{
 
 }

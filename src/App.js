e9ed9d42233ae41b017f55c91cdcccab588133d3
Luis david <david12.29@hotmail.com>
2020-06-06 22:25:36 -0500

    Estoy haciendo cambios en cart.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./app.css";
+import Cart  from "./components/cart";
 
 function App() {
   return (
     <div>
       <h1>I am going the best car in the word.</h1>
+      <Cart/>
     </div>
   );
 }
---
+++ src/components/cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+class Cart extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Hola mundo</h2>
+            </div>        
+        );
+    }
+              
+}
+export default Cart;

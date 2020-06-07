646f66159e2eda3ff9b7ca5df6f6a206e2a06f32
Luis david <david12.29@hotmail.com>
2020-06-07 18:04:39 -0500

    Estoy poniendo los elementos en items

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -3,7 +3,18 @@ import Item2 from '../../images/fresas.jpg'
 import Item3 from '../../images/magos.jpg'
 import Item4 from '../../images/peras.jpg'
 import Item5 from '../../images/sandia.jpg'
-const cartReducer=(state=initState,action)=>{
-
+const initState={
+    items:[
+        {id:1,title:'Banana', desc: "soy una banana", price:110,img:Item1},
+        {id:2,title:'fresas', desc: "somos fresas", price:210,img:Item2},
+        {id:3,title:'Mangos', desc: "soy una mango", price:110,img:Item3},
+        {id:4,title:'Peras', desc: "somos peras", price:510,img:Item4},
+        {id:1,title:'Sandia', desc: "mira soy una sandia", price:510,img:Item5},
+    ],
+    addedItems:[],
+    total: 0
 }
+const cartReducer=(state=initState,action)=>{
+    
+}    
 export default cartReducer;
